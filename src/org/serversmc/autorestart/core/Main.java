67907206d71d924451be6d1a08aba2eab017a960
Dennis Lysenko <dennislysenko@hotmail.com>
2015-08-02 00:58:52 -0700

    Added more changes, and features

--- src/org/serversmc/autorestart/core/Main.java
+++ src/org/serversmc/autorestart/core/Main.java
@@ -1,13 +1,86 @@
 package org.serversmc.autorestart.core;
 
+import java.io.File;
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.serversmc.autorestart.commands.CmdAutoRestart;
+import org.serversmc.autorestart.utils.Config;
+import org.serversmc.autorestart.utils.MemoryUtils;
 
 public class Main extends JavaPlugin {
 
+	public static final Integer TIMED = 0;
+	public static final Integer FORCED = 1;
+	public static final Integer DELAYED = 2;
+	
+	public Logger log = Bukkit.getLogger();
+	
 	@Override
 	public void onEnable() {
-		new Thread(new TimerThread()).start();
+		// Pre Startup Stuff
+		setupFiles();
+		saveConfig();
+		updateConfig();
+		Config.setConfig(getConfig());
+		if (new Config().isMainMulticraft()) {
+			MemoryUtils.setMutlticraft();
+		}
 		
+		// Command Setup
+		getCommand("autore").setExecutor(new CmdAutoRestart());
+		
+		// Loop Starter
+		new Thread(new TimerThread()).start();
+	}
+	
+	public void setupFiles() {
+		List<File> folders = new ArrayList<File>();
+		folders.add(getDataFolder());
+		for (File file : folders) {
+			if (!file.exists()) {
+				file.mkdirs();
+			}
+		}
+		List<String> resources = new ArrayList<String>();
+		resources.add("config.yml");
+		for (String resource : resources) {
+			saveResource(resource, false);
+		}
+	}
+	
+	public void updateConfig() {
+		Integer configVersion = getConfig().getDefaults().getInt("version");
+		Integer configVersionLoaded = new Config().getVersion();
+		if (configVersion != configVersionLoaded) {
+			log.warning("[AutoRestart] the config has updated since the last update!");
+			while (true) {
+				File config = new File(getDataFolder(), "config.yml");
+				Integer r = new SecureRandom().nextInt(100);
+				File rename = new File(getDataFolder(), "config_bkp" + r + ".yml");
+				if (!rename.exists()) {
+					config.renameTo(rename);
+					break;
+				}
+				log.warning("[AutoRestart] config file has been backed up to " + rename.getName() + "!");
+			}
+		}
+	}
+	
+	public static void shutdownServer(Integer action) {
+		if (action == TIMED) {
+			
+		}
+		else if (action == FORCED) {
+			
+		}
+		else if (action == DELAYED) {
+			
+		}
 	}
 	
 }
--- src/org/serversmc/autorestart/core/TimerThread.java
+++ src/org/serversmc/autorestart/core/TimerThread.java
@@ -4,28 +4,61 @@ import java.util.List;
 
 import org.bukkit.Bukkit;
 import org.serversmc.autorestart.utils.Config;
-import org.serversmc.autorestart.utils.TitleLib;
+import org.serversmc.autorestart.utils.MemoryUtils;
+import org.serversmc.autorestart.utils.Messenger;
 
 public class TimerThread implements Runnable {
 
+	private static Config config = new Config();
 	private static Integer time = 0;
 	private static Boolean running = true;
-	
+
 	@Override
 	public void run() {
-		List<Integer> reminders = Config.getReminders();
-		time = Config.getTime();
+		List<Integer> reminders = config.getReminderTimerMintues();
+		time = (int) (config.getMainInterval() * 3600);
 		while (true) {
 			if (time > 0) {
 				if (running) {
+					// Minute Reminders
 					for (Integer reminder : reminders) {
-						Integer minute = time / 60;
-						if (minute == reminder) {
-							TitleLib.broadcastReminder(minute.toString());
-							Bukkit.broadcastMessage(Config.getMessageMinutes(minute.toString()));
+						if (time == reminder * 60) {
+							Messenger.popupMinutes(reminder);
+							Messenger.broadcastMinutes(reminder);
 						}
 					}
+
+					// Second Reminders
+					if (time == config.getReminderTimerSeconds()) {
+						Messenger.popupSeconds(time);
+						Messenger.broadcastSeconds(time);
+					}
+
+					// Commands Exector
+					if (config.isCommandsEnabled()) {
+						if (time == config.getCommandsTime()) {
+							for (String command : config.getCommandsList()) {
+								if (command.startsWith("/")) {
+									command = command.replaceFirst("/", "");
+								}
+								Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command);
+							}
+						}
+					}
+
 					time--;
+				}
+				else {
+					while (true) {
+						if (Bukkit.getOnlinePlayers().size() > config.getMaxplayersAmount()) {
+							if (config.isMaxplayersEnabled()) {
+								MemoryUtils.setWaiting();
+								Messenger.broadcastMaxplayersCanceled();
+								break;
+							}
+						}
+						Main.shutdownServer(Main.TIMED);
+					}
 				}
 			}
 			try {
@@ -35,5 +68,5 @@ public class TimerThread implements Runnable {
 			}
 		}
 	}
-	
+
 }
