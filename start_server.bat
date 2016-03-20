306bfed48549b7ce4b694dc1dd2d5f152cdddcdf
Dennis Lysenko <dennislysenko@hotmail.com>
2016-03-20 05:57:02 -0400

    Added comments in BASH and SH files

--- start_server.bat
+++ start_server.bat
@@ -1,4 +1,6 @@
+:: /// Put this file in the same folder as your Spigot.jar, or your CraftBukkit.jar file /// ::
+:: /// Use this file to start your server, if you do not have MultiCraft /// ::
 @echo off
 :start
-java -jar Spigot.jar
+java -jar Spigot.jar :: // Change this file name to your jar file name /// ::
 goto start
