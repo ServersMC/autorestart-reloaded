370f228c6dbea39ad1a5b5a4cbf57de388d083fc
Dennis Lysenko <dennislysenko@hotmail.com>
2016-03-20 07:03:00 -0400

    Removed unnecessary comments, and refined instructions.txt

--- start_server.sh
+++ start_server.sh
@@ -1,6 +1,4 @@
-## /// Put this file in the same folder as your Spigot.jar, or your CraftBukkit.jar file /// ##
-## /// Use this file to start your server, if you do not have MultiCraft /// ##
 while true
 do
-   java -jar Spigot.jar ## // Change this file name to your jar file name /// ##
+   java -jar FILENAME
 done
